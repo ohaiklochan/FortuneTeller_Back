ef687caf74f60c2a2485bdfbcdd0961b893ee24a
Ohaiklochan <caforonda103@yahoo.com>
2022-02-15 17:30:55 -0600

    updated routes

--- fortune_api/config/routes.rb
+++ fortune_api/config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :cookies
+      resources :cards
     end
   end
   namespace :api do
     namespace :v1 do
-      resources :cards
+      resources :cookies
     end
   end
   
